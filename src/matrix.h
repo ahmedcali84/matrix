059f9c07b2e6601b58153f2516ae3dd19fc83dec
Ahmed Ali <ahmedcali1984@gmail.com>
2024-08-03 22:31:13 +0300

    Added comments for Function declarations

--- src/matrix.h
+++ src/matrix.h
@@ -16,7 +16,7 @@ typedef struct Matrix
 	float ncols;
 } Matrix;
 
-
+// Allocates Memory for a new Matrix
 Matrix allocate(float rows , float cols);
 
 // Generates random Matrix(nrows x ncols)
@@ -33,22 +33,35 @@ Matrix  matrix_add(Matrix *A , Matrix *B);
 
 // Subtracts two Matrices
 Matrix  matrix_sub(Matrix *A , Matrix *B);
+
+// Computes Element-wise Product of Two Matrices
 Matrix Hadamard_Product(Matrix * A, Matrix * B);
-// Multiplies two Matrices
+
+// Computes the Dot-Product of  two Matrices
 Matrix dot_product(Matrix * A, Matrix * B);
+
+// Returns the Inverse of a Matrix (in-Works , will update when its finished)
 Matrix inverse(Matrix *A);
+
 // Return Transpose of Matrix A
 Matrix Transpose(Matrix *A);
 
 // Test Matrices
 void Test_Matrix(Matrix A , Matrix B , char *matrix_a , char *matrix_b);
+
+// Special Macro Version of Test Matrix Function also prints the variable-name of the Matrix
 #define TEST_MATRIX(A , B) Test_Matrix(A , B , #A ,#B)
 
+// Creates A Matrix of n,... , n+1 Matrix (For Development Purposes)
 Matrix create_matrix(float nrows, float ncols);
 
+// Returns the doubled-version of created Matrix (For Development Purposes)
 Matrix expected_matrix(Matrix *input);
 
+// Computes the cost Function (For Development Purposes)
 float cost(Matrix *p , Matrix *y);
+
+// Trains a simple neural network using gradient descent (For Development Purposes)
 void train(Matrix *x , Matrix *w , Matrix *y , int epochs , float learn , int max);
 
 // Function to free Allocated Memory
