2336e87456da7c5ab7d13ba3e17c9aab2e15ae9e
Ahmed Ali <ahmedcali1984@gmail.com>
2024-08-07 20:16:11 +0300

    Add define ANSI ESCAPE CODES

--- src/matrix.h
+++ src/matrix.h
@@ -9,6 +9,14 @@
 #include <stdbool.h>
 
 
+// Define escape codes for text formatting
+#define RESET   "\033[0m"
+#define BOLD    "\033[1m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+
+
 // Matrix Struction and Information
 typedef struct Matrix
 {
