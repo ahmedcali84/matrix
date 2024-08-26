721e5ef677e0e7addcae60c175afccad4ebc8d0c
Ahmed Ali <ahmedcali1984@gmail.com>
2024-08-26 19:24:48 +0300

    new test cases

--- src/main.c
+++ src/main.c
@@ -13,21 +13,27 @@ int main(int argc , char **argv)
 
 	int max_value = atoi(argv[1]);
 
-	Matrix V1 = random_matrix(max_value , 1 , 20);
-	Matrix V2 = random_matrix(max_value , 1 , 20);
-	Matrix V_T  = Transpose(&V2);
+	Matrix V1 = random_matrix(max_value , 1 , 3);
+	Matrix V2 = random_matrix(max_value , 1 , 3);
+	Matrix V2_T  = Transpose(&V2);
 
-	Matrix V3_dot = dot_product(&V1 , &V_T);
+	// Matrix V1_V2_dot = dot_product(&V1 , &V2);
+	Matrix V1_V2_T_dot = dot_product(&V1 , &V2_T);
+	Matrix V2_V2_T_dot = dot_product(&V2 , &V2_T);
 
 	MATRIX_PRINT(V1);
 	MATRIX_PRINT(V2);
-	MATRIX_PRINT(V_T);
-	MATRIX_PRINT(V3_dot);
+	MATRIX_PRINT(V2_T);
+	// MATRIX_PRINT(V1_V2_dot);
+	MATRIX_PRINT(V1_V2_T_dot);
+	MATRIX_PRINT(V2_V2_T_dot);
 
 	unload(&V1);
 	unload(&V2);
-	unload(&V_T);
-	unload(&V3_dot);
+	unload(&V2_T);
+	// unload(&V1_V2_dot);
+	unload(&V1_V2_T_dot);
+	unload(&V2_V2_T_dot);
 
 	return 0;
 }
