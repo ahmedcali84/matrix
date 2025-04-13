b0faea68ce0fd3c9cd6b565c322ea9786cbbe2c6
Ahmed Ali <ahmedcali1984@gmail.com>
2025-04-13 11:52:14 +0300

    Add Tests

--- test/test_doubles.c
+++ test/test_doubles.c
@@ -19,23 +19,52 @@ void run_tests(Test_Case test_cases[], unsigned int num_tests)
 
 void TEST_RANDOM_MATRIX()
 {
-    Matrix A = random_matrix(4, 4);
-    assert(A.A != NULL && "ERROR: Test Failed.\n");
-    PRINT(A);
+    Matrix A = random_matrix(1000, 500);
+    assert(A.A != NULL && "ERROR: Test Random Matrix Failed.\n");
     unload(&A);
+}
+
+void TEST_MATRIX_ADDITION()
+{
+    Matrix A = fill(100, 50, 100.0);
+    Matrix B = fill(100, 50, 100.0);
+    
+    Matrix C = matrix_add(&A, &B);
+    double value = get_element(C, 0, 0);
+    assert(value == 200.0 && "ERROR: Test Matrix Addition Failed.\n");
+    
+    unload(&A);
+    unload(&B);
+    unload(&C);
+}
+
+void TEST_MATRIX_SUBTRACTION()
+{
+    Matrix A = fill(100, 50, 100.0);
+    Matrix B = fill(100, 50, 50.0);
+    
+    Matrix C = matrix_subtract(&A, &B);
+    double value = get_element(C, 0, 0);
+    assert(value == 50.0 && "ERROR: Test Matrix Addition Failed.\n");
+    
+    unload(&A);
+    unload(&B);
+    unload(&C);
 }
 
 int main(void)
 {
     srand(time(NULL));
-    printf("INFO: Testing Matrix Functions of TYPE DOUBLES....\n");
+    printf("INFO: Testing Matrix Functions...\n");
     Test_Case test_cases[] = {
-        {"RANDOM_MATRIX", TEST_RANDOM_MATRIX},
+        {"TEST MATRIX RANDOM",      TEST_RANDOM_MATRIX},
+        {"TEST MATRIX ADDITION",    TEST_MATRIX_ADDITION},
+        {"TEST MATRIX SUBTRACTION", TEST_MATRIX_SUBTRACTION},
     };
     
     unsigned int num_tests = ARRAY_LEN(test_cases);
     run_tests(test_cases, num_tests);
 
-    printf("INFO: Testing for Matrix functions of TYPE DOUBLES Done.\n\n");
+    printf("INFO: Testing for Matrix functions Done.\n\n");
     return 0;
 }
