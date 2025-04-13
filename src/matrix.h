2097f4442a901be5bac88c30eaf9befcef059f82
Ahmed Ali <ahmedcali1984@gmail.com>
2025-04-13 17:03:29 +0300

    src/matrix.c

--- src/matrix.h
+++ src/matrix.h
@@ -22,8 +22,6 @@ extern "C" {
 #include <math.h>
 #include <stdbool.h>
 #include <limits.h>
-#include <float.h>
-#include <string.h>
 #include <assert.h>
 
 #define MATDEF extern
@@ -35,6 +33,11 @@ typedef struct Matrix {
     size_t ncols; // Number of columns in the matrix
 } Matrix;
 
+typedef struct _shape {
+    size_t rows;
+    size_t cols;
+} Shape;
+
 /*
 ************** MATRIX OPERATIONS FUNCTIONS DECLARATIONS *******************
 */
@@ -44,21 +47,23 @@ MATDEF void set_element(Matrix C, size_t row, size_t col, double value);        
 MATDEF Matrix create_matrix(size_t rows, size_t cols);                              // creates an empty Matrix with specified parameters
 MATDEF Matrix random_matrix(size_t nrows, size_t ncols);                            // generates random Matrix(nrows x ncols)
 MATDEF void print_matrix(const Matrix b, const char *name);                         // prints a Matrix
-MATDEF int matrix_shape(Matrix a, const char *name);                                // prints the shape of the Matrix
+MATDEF Shape matrix_shape(Matrix A);                                                // prints the shape of the Matrix
 MATDEF Matrix matrix_add(Matrix *a, Matrix *b);                                     // adds two matrices
 MATDEF Matrix matrix_subtract(Matrix *a, Matrix *b);                                // subtracts two matrices
 MATDEF Matrix hadamard_product(Matrix *a, Matrix *b);                               // computes element-wise product of two matrices
 MATDEF Matrix dot_product(Matrix *a, Matrix *b);                                    // computes the dot-product of two matrices
 MATDEF Matrix transpose(Matrix *a);                                                 // return transpose of Matrix a
-MATDEF bool test_matrix_equal(Matrix a, Matrix b, char *Matrix_a, char *Matrix_b);  // test matrices for equality
+MATDEF bool test_matrix_equal(Matrix a, Matrix b);                                  // test matrices for equality
 MATDEF Matrix fill(size_t nrows, size_t ncols, double fill_value);                  // creates a Matrix filled with a specific value (for development purposes)
 MATDEF void unload(Matrix *b);                                                      // Function to free Allocated Memory
 
 // Special Macro Functions
 #define PRINT(B) print_matrix(B, #B)                                /* Macro definition of a special print function */
-#define SHAPE(A) matrix_shape(A, #A)                                /* Macro definition of a special SHAPE printing function */
-#define TEST_MATRIX(A, B) test_matrix_equal(A, B, #A, #B)           /* Special Macro Version of Test Matrix Function also prints the variable-name of the Matrix */
-#define ALLOCATION_FAILED "Memory Allocation Failed.\n"
+#define ALLOCATION_FAILED "Memory Allocation Failed.\n"             /* Return Error String */
+
+#define PRINT_SHAPE(A)\
+    Shape shape = matrix_shape(A);\
+    printf("Shape: [%zu, %zu]\n", shape.rows, shape.cols);
 
 #ifdef __cplusplus
 }
